b4d43b51dac606b653cd9638a251915c7f6f71ef
johanncanon <johanncanon@gmail.com>
2025-02-06 14:43:10 -0500

    initial commit

---
+++ src/main/java/com/johanncanon/desingpatterns/DesingPatternsImplementationApplication.java
@@ -0,0 +1,13 @@
+package com.johanncanon.desingpatterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesingPatternsImplementationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesingPatternsImplementationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/johanncanon/desingpatterns/DesingPatternsImplementationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.johanncanon.desingpatterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesingPatternsImplementationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
